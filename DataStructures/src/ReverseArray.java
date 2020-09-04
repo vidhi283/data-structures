cb6724327356614640fd7e0b200363c9836b9d32
vidhi283 <shahvidhik@gmail.com>
2020-09-04 05:58:30 -0700

    reverse array completed

--- DataStructures/src/ReverseArray.java
+++ DataStructures/src/ReverseArray.java
@@ -1,8 +1,33 @@
 public class ReverseArray {
-	
-	public static void main(String args[])
-	{
-		System.out.println("test");
-	}
+
+	    static int[] reverseArray(int[] a) 
+	    {	
+	    	//new reversed array declaration with length same as input array
+	    	int[] reversed_nums = new int[a.length];
+	    	//variable j for reversed array
+	    	int j=0;
+	    	//for loop to traverse through the old array from last element
+	    	for (int i = a.length -1; i>=0; i--)
+	    	{
+	    		//new array elements from old array from last element
+	    		reversed_nums[j] = a[i];
+	    		//incrementing j
+	    		j++;
+	    		//print elements a[i]
+	    		System.out.print(a[i]);
+	    		//print space between array elements
+	    		System.out.print(" ");
+	    	}
+	    	//returning reversed array
+	    	return reversed_nums;
+	    }
+	    
+	    public static void main(String args[])
+	    {
+	    	//original array
+	    	int[] a = {7,8,4,9};
+	    	//calling the reversedArray function in main
+	    	reverseArray(a);
+	    }
 
 }
